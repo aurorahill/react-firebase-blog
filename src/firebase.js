3c38a460c1fed66456e774b9655785d1f148d9eb
Bartosz Słysz <bartek.slysz23@gmail.com>
2025-03-23 17:33:56 +0100

    refactor: clean up Firebase configuration and remove unused files
    - Removed hardcoded Firebase configuration in favor of environment variables for better security.
    - Deleted unused logo.svg file and cleaned up comments in index.js for improved readability.

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCkpJAA33RO2oZ8TnQifJeWFQd1REK6Mm8",
-//   authDomain: "react-blog-app-c9c57.firebaseapp.com",
-//   projectId: "react-blog-app-c9c57",
-//   storageBucket: "react-blog-app-c9c57.firebasestorage.app",
-//   messagingSenderId: "181138401911",
-//   appId: "1:181138401911:web:555d6fd85008daffbcf472",
-// };
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
